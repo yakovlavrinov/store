17156c3d4c1653b90b2386f0ad68e24d655f5616
Yakov <lawrinov.yackov@yandex.ru>
2023-04-12 20:38:01 +0300

    sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import styles from '../../styles/Sidebar.module.css';
+
 
 const Sidebar = () => {
     return (
-        <div>
-            Sidebar
-        </div>
+       <section className={styles.sidebar}>
+            <div className={styles.title}>CATEGORIES</div>
+            <nav>
+                <ul className={styles.menu}>
+                    <li>
+                        <NavLink to={`/categories/${1}`}>
+                            Link
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className={styles.footer}>
+                <a href="/help" target='_blank' className={styles.link}>
+                    Help
+                </a>
+
+                <a 
+                    href="/terms"
+                    target='_blank'
+                    className={styles.link}
+                    style={{textDecoration: 'underline'}}
+                >
+                    Terms & Conditions
+                </a>
+            </div>
+       </section>
     );
 };
 
