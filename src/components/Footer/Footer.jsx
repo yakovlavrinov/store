133b14b4bd3e3fc5b949ecb3f9949bdc8994e9d7
Yakov <lawrinov.yackov@yandex.ru>
2023-04-12 20:13:55 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+import styles from '../../styles/Footer.module.css';
+
+import { ROUTES } from '../../utils/routes';
+
+import LOGO from '../../images/logo.svg';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <section className={styles.footer}>
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="Stuff" />
+                </Link>
+            </div>
+
+            <div className={styles.rights}>
+                Developed by YAKOV
+            </div>
+
+            <div className={styles.socials}>
+                <svg className="icon">
+                    <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+                </svg>
+
+                <svg className="icon">
+                    <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+                </svg>
+
+                <svg className="icon">
+                    <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+                </svg>
+            </div>
+        </section>
     );
 };
 
