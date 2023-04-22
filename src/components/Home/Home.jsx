fb923d0699b7562260715e05b7c6cd502f90449a
Yakov <lawrinov.yackov@yandex.ru>
2023-04-22 11:55:31 +0300

    banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Poster from '../Poster/Poster';
 import Products from '../Products/Products';
 import { useSelector } from 'react-redux';
 import Categories from '../Categories/Categories';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
 
@@ -13,6 +14,7 @@ const Home = () => {
             <Poster/>
             <Products products={products.list} amount={5} title="Trending"/>
             <Categories products={categories.list} amount={5} title="Worth seeing" />
+            <Banner/>
         </>
     );
 };
